74b2a3c92fd60ab5af276af7ec532272f40f60d6
Oligerta <oligerta.bb@gmail.com>
2026-09-01 23:58:55 +0200

    Sidebar

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from '@tanstack/react-router'
 import { Navbar } from '../components/Navbar'
+import { Sidebar } from '../components/Sidebar'
+import styles from '../components/Sidebar.module.css'
 
 export function MainLayout() {
     return(
         <>
         <Navbar />
-        <main>
+        <Sidebar />
+        <main className={styles.content}>
             <Outlet />
         </main>
         </>
