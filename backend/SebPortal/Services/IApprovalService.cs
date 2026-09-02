314a1e36cd5c975336b9956069729c2f31db3352
niklas eriksson <niklaseriksson32@hotmail.com>
2026-09-02 11:11:43 +0200

    Add Service classes for Payment Creation

---
+++ backend/SebPortal/Services/IApprovalService.cs
@@ -0,0 +1,12 @@
+﻿using SebPortal.Models;
+
+namespace SebPortal.Api.Services
+{
+    public interface IApprovalService
+    {
+        bool IsApprovalStepValid(Payment payment, ApprovalStep approvalStep, int currentUserId);
+        Task<ApprovalStep> GetApprovalStepByIdAsync(int id);
+        Task<IEnumerable<ApprovalStep>> GetApprovalStepsByPaymentIdAsync(int paymentId);
+        Task<ApprovalStep> UpdateApprovalStepAsync(ApprovalStep approvalStep);
+    }
+}
