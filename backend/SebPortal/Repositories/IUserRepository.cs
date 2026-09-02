e52a72d0b5f4b9749aa2ca76b168a01196932908
niklas eriksson <niklaseriksson32@hotmail.com>
2026-09-02 11:11:19 +0200

    Add user Repository logic

---
+++ backend/SebPortal/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using SebPortal.Models;
+
+namespace SebPortal.Api.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByIdAsync(int userId);
+        Task<User> GetUserByEmailAsync(string email);
+        Task<User> CreateUserAsync(User user);
+        Task<User> UpdateUserAsync(User user);
+        Task<bool> DeleteUserAsync(int userId);
+    }
+}
