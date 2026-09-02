8b156a4d436898decf06d393e4d7bae1dd8d1ddb
niklas eriksson <niklaseriksson32@hotmail.com>
2026-09-02 12:40:45 +0200

    Add [Required] to Models for consistency

--- backend/SebPortal/Models/AuditEntries.cs
+++ backend/SebPortal/Models/AuditEntries.cs
@@ -6,13 +6,16 @@ namespace SebPortal.Models
     {
         public int Id { get; set; }
         public int UserId { get; set; }
-        public User User { get; set; } // Navigation property to Users
+        public User User { get; set; } = null!; // Navigation property to Users
+        [Required]
         [MaxLength(100)]
-        public string Action {  get; set; }
+        public required string Action {  get; set; }
+        [Required]
         [MaxLength(50)]
-        public string EntityType { get; set; }
+        public required string EntityType { get; set; }
         public int EntityId { get; set; }
-        public string Description { get; set; }
+        [Required]
+        public required string Description { get; set; }
         public DateTime DateTime { get; set; } = DateTime.Now;
     }
 }
