ea82010f357bbeb14c877c958b6ab314cdd39d1f
niklas eriksson <niklaseriksson32@hotmail.com>
2026-09-02 11:10:05 +0200

    Add user navigation to Audit model

--- backend/SebPortal/Models/AuditEntries.cs
+++ backend/SebPortal/Models/AuditEntries.cs
@@ -6,6 +6,7 @@ namespace SebPortal.Models
     {
         public int Id { get; set; }
         public int UserId { get; set; }
+        public User User { get; set; } // Navigation property to Users
         [MaxLength(100)]
         public string Action {  get; set; }
         [MaxLength(50)]
