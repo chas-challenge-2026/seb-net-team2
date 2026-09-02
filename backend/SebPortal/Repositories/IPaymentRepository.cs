aa43dce21a23abf022fb6412d491f980371ea134
niklas eriksson <niklaseriksson32@hotmail.com>
2026-09-02 11:11:03 +0200

    Add payment Repository logic

---
+++ backend/SebPortal/Repositories/IPaymentRepository.cs
@@ -0,0 +1,12 @@
+﻿using SebPortal.Models;
+
+namespace SebPortal.Api.Repositories
+{
+    public interface IPaymentRepository
+    {
+        Task<Payment> CreatePaymentAsync(Payment payment);
+        Task<Payment> GetPaymentByIdAsync(int paymentId);
+        Task<IEnumerable<Payment>> GetPaymentsByUserIdAsync(int userId);
+        Task UpdatePaymentStatusAsync(int paymentId, string status);
+    }
+}
