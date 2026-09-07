73b21aaffc99a3286cfc248c33489139fdb53aec
Mattias Eskilsson <mattiaseskilsson@hotmail.se>
2026-09-07 12:29:17 +0200

    Refactor registration page to support customer types

--- frontend/src/pages/Register/Register.tsx
+++ frontend/src/pages/Register/Register.tsx
@@ -1,5 +1,46 @@
+import { useState } from "react";
+
+import Card from "../../components/Card/Card";
+import CustomerTypeSelector, {
+    type CustomerType,
+} from "../../components/CustomerTypeSelector/CustomerTypeSelector";
+
+import PrivateRegistration from "./PrivateRegistration";
+import CompanyRegistration from "./CompanyRegistration";
+
+import styles from "./Register.module.css";
+
 export default function Register() {
+    const [customerType, setCustomerType] =
+        useState<CustomerType>("private");
+
     return (
-        <h1>Register</h1>
+        <div className={styles.layout}>
+            <Card
+                variant="default"
+                className={styles.registerCard}
+            >
+                <div className={styles.headerSection}>
+                    <h1 className={styles.header}>
+                        Welcome to SEB
+                    </h1>
+
+                    <div className={styles.line} />
+                </div>
+
+                <div className={styles.selectorContainer}>
+                    <CustomerTypeSelector
+                        value={customerType}
+                        onChange={setCustomerType}
+                    />
+                </div>
+
+                {customerType === "private" ? (
+                    <PrivateRegistration />
+                ) : (
+                    <CompanyRegistration />
+                )}
+            </Card>
+        </div>
     );
 }
