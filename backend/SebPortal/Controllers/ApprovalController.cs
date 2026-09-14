544ad671136e8ae56a39d01f5602144c3135f68e
niklaserikssoon <niklas.eriksson@chasacademy.se>
2026-09-14 10:08:47 +0200

    Update ApprovalController.cs
    Change to Capital P in approval payment API route

--- backend/SebPortal/Controllers/ApprovalController.cs
+++ backend/SebPortal/Controllers/ApprovalController.cs
@@ -55,7 +55,7 @@ namespace SebPortal.Api.Controllers
             return int.TryParse(claim, out var userId) ? userId : null;
         }
 
-        [HttpGet("payment/{paymentId}/steps")]
+        [HttpGet("Payment/{paymentId}/steps")]
         public async Task<IActionResult> GetApprovalStepsForPayment(int paymentId)
         {
             var userId = GetCurrentUserId();
