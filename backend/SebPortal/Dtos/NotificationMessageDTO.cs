01f4e8a2a9c052580964f39606a7e1196aa5255a
Lisa Ebbhagen <lisa.ebbhagen@gmail.com>
2026-09-15 15:56:59 +0200

    added model and dto for Notification, updated db context.

---
+++ backend/SebPortal/Dtos/NotificationMessageDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace SebPortal.Api.Dtos
+{
+    public class NotificationMessageDTO
+    {
+        public string RecipientEmail { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public int AttemptCount { get; set; } = 0;
+    }
+}
