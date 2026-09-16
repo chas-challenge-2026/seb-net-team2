bea97290ea1d0ad9e09152349368776f960c3320
Lisa Ebbhagen <lisa.ebbhagen@gmail.com>
2026-09-16 11:35:20 +0200

    m

---
+++ backend/SebPortal/Services/NotificationService.cs
@@ -0,0 +1,32 @@
+﻿using SebPortal.Api.Repositories;
+using System.Net.Mail;
+using SebPortal.Api.Dtos;
+using Polly;
+using Polly.Retry;
+using Microsoft.Extensions.Logging;
+
+namespace SebPortal.Api.Services
+{
+    public class NotificationService : INotificationService
+    {
+        private readonly INotificationRepository _repository;
+        private readonly ILogger<NotificationService> _logger;
+        private readonly SmtpClient _smtpClient;
+
+        public NotificationService(INotificationRepository repository, ILogger<NotificationService> logger, SmtpClient smtpClient )
+        {
+            _repository = repository;
+            _logger = logger;
+            _smtpClient = smtpClient;
+        }
+
+        public async Task SendNotificationMessageAsync(NotificationMessageDTO dto)
+        {
+            var pipeline = new ResiliencePipelineBuilder()
+                .AddRetry(new RetryStrategyOptions
+                {
+
+                })
+        }
+    }
+}
