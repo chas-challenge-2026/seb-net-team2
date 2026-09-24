e490a9dfdef0cae6620588da3eb86f91c7008f00
Emilio <xxglobalelitexx123@gmail.com>
2026-09-24 18:12:26 +0200

    [NATIVE] Reworked CSV parser can now parse basic CSV files without a problem

--- native/csv/csv.c
+++ native/csv/csv.c
@@ -18,6 +18,20 @@ static bool is_end_of_line(char *curr)
     return false;
 }
 
+static void add_to_data(Csv* csv, void* data)
+{
+    if (csv->data_length == csv->data_capacity)
+    {
+        size_t previous_size = csv->data_capacity;
+        csv->data_capacity *= 2;
+        csv->data = (void **)realloc(csv->data, sizeof(void *) * csv->data_capacity);
+        memset(&csv->data[csv->data_length], 0, sizeof(void *) * (csv->data_capacity - previous_size));
+    }
+
+    csv->data[csv->data_length] = data;
+    csv->data_length++;
+}
+
 Csv_Error csv_parse(const char* content, int content_len, Csv* out_csv)
 {
     if (content == NULL)
@@ -48,8 +62,6 @@ Csv_Error csv_parse(const char* content, int content_len, Csv* out_csv)
     out_csv->data_capacity = 8;
     out_csv->data = (void**)malloc(sizeof(void*) * out_csv->data_capacity);
     memset(out_csv->data, 0, sizeof(void*) * out_csv->data_capacity);
-                printf("Hello world!\n");
-
 
     while (true)
     {
@@ -62,28 +74,61 @@ Csv_Error csv_parse(const char* content, int content_len, Csv* out_csv)
         if (buffer[current] == ',' || is_eol)
         {//from_account_i
             // text
+
+            //printf("Checking alpha [%c]\n", buffer[start]);
             if (isalpha(buffer[start]) != 0)
             {
+
                 char* text = (char*)malloc(sizeof(char) * (current - start) + 1);
                 memcpy(text, &buffer[start], (current - start));
                 text[(current-start)] = '\0';
                 printf("Hello test: [%s] | length: %d\n", text, current - start );
-
-                if (out_csv->data_length == out_csv->data_capacity)
-                {
-                    size_t previous_size = out_csv->data_capacity;
-                    out_csv->data_capacity *= 2;
-                    out_csv->data = (void**)realloc(out_csv->data, sizeof(void*) * out_csv->data_capacity);
-                    memset(&out_csv->data[out_csv->data_length], 0, sizeof(void*) * out_csv->data_capacity - previous_size); 
-                }
-
-                out_csv->data[out_csv->data_length] = (void*)text;
-                out_csv->data_length++;
+                add_to_data(out_csv, (void*)text);
                 
                 if (!is_eol)
                     start = current + 1; // a comma is just 1 character so we need to add 1.
                 else
+                {
                     start = current + 2; // eol is 2 characters so we need to add 2.
+                    printf("Found EOL[%d]!\n", start);
+                }
+            }
+            else if (isdigit(buffer[start]) != 0)
+            {
+                bool is_decimal = false;
+                for (size_t i = start; i < current; i++)
+                {
+                    if (buffer[i] == '.')
+                    {
+                        is_decimal = true;
+                        break;
+                    }
+                }
+
+                char temp_buffer[current - start + 1];
+                memcpy(temp_buffer, &buffer[start], current - start);
+                temp_buffer[current - start] = '\0';
+
+                printf("Decimal/Digit buffer: [%s]\n", temp_buffer);
+                char* temp;
+
+                if (is_decimal)
+                {
+                    double* decimal_value = (double*)malloc(sizeof(double));
+                    *decimal_value = strtod(temp_buffer, &temp);
+
+                    add_to_data(out_csv, (void*)decimal_value);
+                }
+                else 
+                {
+                    int* value = (int*)malloc(sizeof(int));
+                    *value = strtol(temp_buffer, &temp, 10);
+
+                    add_to_data(out_csv, (void*)value);
+                }
+
+                
+                start = current + 1;
             }
         }
 
--- native/csv/test.c
+++ native/csv/test.c
@@ -5,7 +5,7 @@
 #include "csv.h"
 
 /* A test that will always pass */
-static void csv_test_rows(void **state) {
+static void csv_test_values(void **state) {
     char *content =
             "from_account_id,to_iban,amount,reference\r\n"
             "1,SE8550000000054910000003,5000.00,Faktura #2001\r\n"
@@ -16,12 +16,16 @@ static void csv_test_rows(void **state) {
     Csv_Error error = csv_parse(content, strlen(content), &csv);
 
     assert_true(strcmp((char*)csv.data[0], "from_account_id") == 0);
+    assert_true(strcmp((char*)csv.data[15], "Faktura #2003") == 0);
+    assert_true(*(int*)csv.data[4] == 1);
+    assert_true(*(double*)csv.data[6] == 5000.00);
+    assert_true(csv.data_length == 16);
     csv_free(&csv);
 }
 
 int main(void) {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(csv_test_rows),
+        cmocka_unit_test(csv_test_values),
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
